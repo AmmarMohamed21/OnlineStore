3a28301a02d5bb4a8bbae426e7c8c50bd7075abb
ahmedayman1420 <76254195+ahmedayman1420@users.noreply.github.com>
2021-01-04 20:52:19 +0200

    Update application.py

--- application.py
+++ application.py
@@ -277,10 +277,10 @@ def Transactions():
         
 
         db.execute("INSERT INTO Refunds (RefundID, Price, DateRefunded, TransactionID) VALUES (:RID, :ProPeice, :Date , :TransID)", 
-        RID = TransID*1000 + RefQua *100 + ProID*10 + ProPrice , ProPeice = ProPrice * RefQua , Date = '4/1/2020' , TransID = TransID )######################################
+        RID = TransID*1000 + ProQua *100 + ProID*10 + ProPrice , ProPeice = ProPrice * RefQua , Date = '4/1/2020' , TransID = TransID )######################################
 
         db.execute("INSERT INTO RefundProducts (RefundID, ProductID, Quantity) VALUES ( :RID, :PID, :Qua)",
-        TransID*1000 + RefQua *100 + ProID*10 + ProPrice , PID = ProID , Qua = RefQua
+        TransID*1000 + RefQua *100 + ProID*10 + ProPrice , PID = ProID , Qua = RefQua)
 
 
     return render_template("Transactions.html" ,categories=categories,CustomerInfo = CustomerInfo ,

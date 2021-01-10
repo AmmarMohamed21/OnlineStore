ea6d3a08a79ebec3d2d59fafb990d946a41d97e0
ahmedayman1420 <76254195+ahmedayman1420@users.noreply.github.com>
2021-01-10 12:30:34 +0200

    Update application.py

--- application.py
+++ application.py
@@ -297,7 +297,7 @@ def Transactions():
         Refund_Quantity.append ( db.execute("SELECT Count(Quantity) FROM RefundProducts RP , RefundS R where R.RefundID = RP.RefundID and RP.ProductID = :ProdID and R.TransactionID = :TransaID", 
         ProdID = ProID , TransaID = TransID ))######################################
         
-        if int(Refund_Quantity[0][0]) > (ProQua - RefQua) :
+        if int(Refund_Quantity[0][1][0]) > (ProQua - RefQua) :
             return apology(" Refund Quantity > Product Quantity ")
 
 #############################################################################################################
